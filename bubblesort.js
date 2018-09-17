e3481023eb3b9949c8d59ce7794c91caa1a3e094
Maggie Walker <mags21walker@gmail.com>
2018-09-17 12:16:54 -0400

    Completed bubbleSort function

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,16 @@
-function bubbleSort(array){ 
-    array = [2, 1]; 
-    swap(array[0], array[1]); 
-    swap(0, 1); 
+function bubbleSort(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1; j++) {
+            swap(j, j + 1);
+        }
 
-
-    function swap(first, second){ 
-        if (array[first] > array[second]){ 
-            let smaller = array[second]; 
-            array[second] = array[first]; 
-            array[first] = smaller; 
+    }
+    function swap(first, second) {
+        if (array[first] > array[second]) {
+            let smaller = array[second];
+            array[second] = array[first];
+            array[first] = smaller;
         }
     }
+    return array;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,4 +1,13 @@
+
 describe('Bubble Sort', function(){ 
+    // beforeAll(function(){
+    //     spyOn(bubbleSort, 'swap').andcallThrough();
+    // });
+    // it('will swap this number of times', function(){
+    //     bubbleSort([2, 7, 4]);
+    //     console.log(bubbleSort.swap.calls.count());
+    // })
+
     it('handles an empty array', function(){ 
         expect(bubbleSort([])).toEqual([]); 
     }); 
@@ -7,6 +16,9 @@ describe('Bubble Sort', function(){
     });
     it('handles an array of multiple items', function(){ 
         expect(bubbleSort([5, 2, 7])).toEqual([2, 5, 7]); 
+    });
+    it('handles an array of many many items', function(){ 
+        expect(bubbleSort([5, 2, 7, 9, 12, 4, 6, 2, 100, 22, 13, 21])).toEqual([2, 2, 4, 5, 6, 7, 9, 12, 13, 21, 22, 100]); 
     });
 
 }); 
